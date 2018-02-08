fc339604fb2df5d379e5ff291dad8402e91bb073
Mirko Schicchi <mirko.schicchi@studenti.unitn.it>
2018-02-08 11:25:19 +0100

    Add first client file

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-var port = 3000 || PROC.env.PORT;
+var port = 3000 || process.env.PORT;
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+    console.log("A user has connected");
+});
 
 
 http.listen(port, () => {
     console.log("Server is listening on port " + port);
-})
+});
